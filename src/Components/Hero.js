21db20831417d6366c7890f4494d7c17410dca8a
hanzhang24 <97270729+hanzhang24@users.noreply.github.com>
2022-12-28 20:29:45 -0500

    Dealing With Flex-Box
    Added Gradient, dealing with flex box

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,8 +8,6 @@ const Hero = () => {
         <div className='content'>
             <p>Hello!</p>
             <p>My Name is Han Zhang</p>
-            <p>I am a sophmore at Duke University stuyding Electrical and Computer Engineering, as well as Computer Science</p>
-            <p>My current interests ... </p>
             <button href='/' className='button'>Contact Me</button>
         </div>
     </div>
